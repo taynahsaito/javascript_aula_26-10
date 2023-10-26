8c07b6f353b1942c8da6d23de532488a858a8d2e
TAYNAH TAMIE MASUNAGA SAITO <23.00621-8@maua.br>
2023-10-26 12:35:36 -0300

    javascipt_pt2_26-10

--- previsao_do_tempo.js
+++ previsao_do_tempo.js
@@ -1,5 +1,31 @@
 //dotenv
 require('dotenv').config()
+const axios = require('axios')
 
-//representa o processo do node
-const APPID = process.env.APPID
+// const APPID = process.env.APPID
+// const Q = process.env.Q
+// const UNTS = process.env.UNITS
+// const LANGUAGE = process.env.LANGUAGE
+// const CNT = process.env.CNT
+// const PROTOCOL = process.env.PROTOCOL
+// const URL_BASE = process.env.URL_BASE
+
+// //operador de desestruturação javascript - isso substitui o bloco de código anterior
+const {
+    APPID,
+    Q,
+    UNITS,
+    LANGUAGE,
+    CNT,
+    PROTOCOL,
+    URL_BASE
+} = process.env
+
+//https://api.openweathermap.org/data/2.5/forecast?q=Itu&units=metric&appid=chave&lang=pt-br&cnt=10
+const url = `${PROTOCOL}://${URL_BASE}?q=${Q}&units=${UNITS}&appid=${APPID}&lang=${LANGUAGE}&cnt=${CNT}`
+
+//devolve uma promise
+const minhaPromise = axios.get(url)
+minhaPromise.then((res) => {
+    console.log(res)
+})
